bded965fc4f1e64f533e913666127ae69b7c146b
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-10-03 22:15:39 +0530

    changed activity name
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/test/java/me/rishabhkhanna/tests/MainActivityTest.java
+++ app/src/test/java/me/rishabhkhanna/tests/MainActivityTest.java
@@ -6,14 +6,11 @@ import static org.junit.Assert.*;
  * Created by rishabhkhanna on 02/10/17.
  */
 
-import static me.rishabhkhanna.tests.MainActivityTest.*;
-import static me.rishabhkhanna.tests.kotlinActivity.*;
-
 public class MainActivityTest {
 
     @Test
     public void calcFareWorks(){
-        assertEquals(50,kotlinActivity.calcFare(0,0),0.001);
+        assertEquals(50, KotlinActivity.calcFare(0,0),0.001);
     }
     @Test
     public void calFareWorks(){
